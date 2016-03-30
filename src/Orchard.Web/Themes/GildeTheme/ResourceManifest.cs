b1b10c1f46c3207f2452d4f1f26c01347ebe724a
SeyDutch <ehfransen@live.nl>
2016-03-30 02:32:19 +0200

    Framework conflict

---
+++ src/Orchard.Web/Themes/GildeTheme/ResourceManifest.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.UI.Resources;
+
+namespace GildeTheme {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+            
+           // manifest.DefineScript("Bootstrap").SetUrl("bootstrap-3.3.5/js/bootstrap.min.js", "bootstrap-3.3.5/js/bootstrap.js").SetVersion("3.3.4").SetDependencies("jQuery");
+           // manifest.DefineScript("Custom").SetUrl("custom.js").SetDependencies("jQuery");
+        }
+    }
+}

f377972a22ed242a25111a88b00024b6f8522f9d
SeyDutch <ehfransen@live.nl>
2016-03-31 01:09:23 +0200

    Theme setup

--- src/Orchard.Web/Themes/GildeTheme/ResourceManifest.cs
+++ src/Orchard.Web/Themes/GildeTheme/ResourceManifest.cs
@@ -4,9 +4,11 @@ namespace GildeTheme {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-            
-           // manifest.DefineScript("Bootstrap").SetUrl("bootstrap-3.3.5/js/bootstrap.min.js", "bootstrap-3.3.5/js/bootstrap.js").SetVersion("3.3.4").SetDependencies("jQuery");
-           // manifest.DefineScript("Custom").SetUrl("custom.js").SetDependencies("jQuery");
+
+            manifest.DefineStyle("Styles").SetUrl("Styles.min.css");
+            manifest.DefineStyle("CarouselWidget").SetUrl("Carousel.min.css");
+
+            manifest.DefineScript("ClientBean").SetUrl("ClientBean.js").SetDependencies("jQuery", "Bootstrap", "Custom");
         }
     }
 }
